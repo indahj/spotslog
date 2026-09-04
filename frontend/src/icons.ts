ea0247bbcd0a6a0c48eeac7b058cc7de088cb7db
indahj <indahjuliani87@gmail.com>
2026-09-04 21:31:05 +0700

    feat(frontend): set up Font Awesome, migrate bookmark icon

---
+++ frontend/src/icons.ts
@@ -0,0 +1,5 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHeart as fasHeart, faBookmark as fasBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as farHeart, faBookmark as farBookmark } from "@fortawesome/free-regular-svg-icons";
+
+library.add(fasHeart, fasBookmark, farHeart, farBookmark)
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,11 +6,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { useAuthStore } from './stores/auth'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import "./icons"
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component("FontAwesomeIcon", FontAwesomeIcon)
 
 // Restore the session before mounting
 // so guards and the nav bar see the real auth state on first paint instead of flashing a logged-out shell.
